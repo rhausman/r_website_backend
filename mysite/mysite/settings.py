f23ca88574e40d7e97138c0771958846fb42d322
Richard Hausman <richard.hausman@thenotcompany.com>
2020-09-08 15:07:33 -0400

    disabled cors credentials

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3030',
     'http://localhost:8000'
